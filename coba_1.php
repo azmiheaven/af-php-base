e0cc972c76c9f0895c6aa4fc2ca6f2dc76543b32
azmiheaven <azmi.heaven@gmail.com>
2013-10-21 13:57:44 +0700

    Create coba_1.php

---
+++ coba_1.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Coba Aplikasi</title>
+</head><body bgcolor=”#FFFFFF” text=”#000000?>
+<p>Kayaknya <?php print $NamaAnda; ?>
+<p>gak bakalan bisa jadian sama
+<b> <?php print $NamaDia; ?> deh!?! </b>
+<p>Cobalah berusaha lebih keras ya…siapa tahu ada kesempatan.
+</body>
+</html>
